221b12d4ec5e452f900150c438427eaa00b75d2d
onnif <onni.forsell@gmail.com>
2022-09-04 17:31:38 +0300

    Lisätty piste tehtävään 3.4

--- Viikko2/teht_3.4.py
+++ Viikko2/teht_3.4.py
@@ -3,4 +3,4 @@ vuosi = int(input("Kirjoita vuosi: "))
 if vuosi % 4 == 0 and vuosi % 100 != 0 or vuosi % 400 == 0:
     print(f"{vuosi} on karkausvuosi!")
 else:
-    print(f"{vuosi} ei ole karkausvuosi")
+    print(f"{vuosi} ei ole karkausvuosi.")
