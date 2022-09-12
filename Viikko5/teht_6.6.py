791524572ac3029676995a09ce584529cca97472
onnif <onni.forsell@gmail.com>
2022-09-12 09:15:54 +0300

    Moduuli Kuusi Tehtävät

--- Viikko5/teht_6.6.py
+++ Viikko5/teht_6.6.py
@@ -1,20 +1,20 @@
 import math
-
 def pizzaLaskin(halk, hin):
     säde = halk / 2
     pinta_ala = math.pi * säde ** 2
     pizzanHinta = hin / pinta_ala
     return pizzanHinta
 
-halkaisija = int(input("Kuinka monta senttiä on pizzan halkaisija? "))
-hinta = float(input("Kuinka monta euroa se maksaa? "))
-pizzaYks = pizzaLaskin(halkaisija, hinta)
+pizzat = []
+pizzaRange = int(input("Kuinka monta pizzaa? "))
+for x in range(pizzaRange):
+    halkaisija = int(input("Kuinka monta senttiä on pizzan halkaisija? "))
+    hinta = float(input("Kuinka monta euroa se maksaa? "))
+    pizzat.append(pizzaLaskin(halkaisija, hinta))
 
-halkaisija = int(input("Kuinka monta senttiä on pizzan halkaisija? "))
-hinta = float(input("Kuinka monta euroa se maksaa? "))
-pizzaKaks = pizzaLaskin(halkaisija, hinta)
+arvokkain = 0
+for x in range(len(pizzat)):
+    if pizzat[arvokkain] > pizzat[x]:
+        arvokkain = x
 
-if pizzaYks < pizzaKaks:
-    print("Pizza 1 on edullisempi")
-else:
-    print("Pizza 2 on edullisempi")
+print(f"Edullisin pizza on pizza {arvokkain+1} ")
